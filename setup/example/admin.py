0e4e97f8e482306c0c8b823997dcae17c766efd2
unknown <jyothiprakashpatnaik@gmail.com>
2021-10-12 14:58:15 +0530

    deleting stuff

--- setup/example/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Campus, Institue, UnderGraduates, PostGraduates
-# Register your models here.
-
-admin.site.register(Campus)
-admin.site.register(Institue)
-admin.site.register(UnderGraduates)
-admin.site.register(PostGraduates)
