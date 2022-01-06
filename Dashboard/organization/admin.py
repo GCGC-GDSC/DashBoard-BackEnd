ae20431003940e17a1bba1ddc9578563f23b58ba
unknown <jyothiprakashpatnaik@gmail.com>
2022-01-06 19:48:48 +0530

    Added Stream Model

--- Dashboard/organization/admin.py
+++ Dashboard/organization/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import (Institute, Campus)
+from .models import (Institute, Campus, Stream)
 
-
+admin.site.register(Stream)
 admin.site.register(Campus)
 admin.site.register(Institute)
