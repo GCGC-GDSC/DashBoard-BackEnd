7f85c35e026a085ca53d80bd6726e47a9994427c
unknown <jyothiprakashpatnaik@gmail.com>
2022-01-29 19:59:39 +0530

    fix Jwt

--- Dashboard/Dashboard/settings.py
+++ Dashboard/Dashboard/settings.py
@@ -194,9 +194,9 @@ REST_FRAMEWORK = {
         # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    'DEFAULT_PARSER_CLASSES': [
-        'rest_framework.parsers.FileUploadParser',
-    ]
+    # 'DEFAULT_PARSER_CLASSES': [
+    #     'rest_framework.parsers.FileUploadParser',
+    # ]
 }
 
 SIMPLE_JWT = {
