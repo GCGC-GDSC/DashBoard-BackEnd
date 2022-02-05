63fe32685fff0675f32b96e441bd42e4cd6a65f0
Dheerajdoppalapudi <dheerudoppalapudi@gmail.com>
2022-02-06 00:14:58 +0530

    token creation added

--- Dashboard/account/serializers.py
+++ Dashboard/account/serializers.py
@@ -2,9 +2,10 @@ from rest_framework import serializers, status
 from .models import *
 from organization.serializers import *
 from organization.models import *
+from rest_framework.authtoken.models import Token
 
 
-class AccountSerialize(serializers.ModelSerializer):
+class UserSerialize(serializers.ModelSerializer):
     can_edit = serializers.SerializerMethodField('_can_edit')
     campus = serializers.SerializerMethodField('_campus_name')
     institute = serializers.SerializerMethodField('_institute')
@@ -20,19 +21,23 @@ class AccountSerialize(serializers.ModelSerializer):
         qs = Campus.objects.filter(name=obj.university)
         return CampusSerializeParse(qs, many=True).data
 
-    def _institute(self, obj):
-        if obj.university != "univ":
-            campus = Campus.objects.filter(name=obj.university)
-        else:
-            return InstituteSerializeParse(Institute.objects.all(),
-                                           many=True).data
+    # def _institute(self, obj):
+    #     if obj.university != "univ":
+    #         campus = Campus.objects.filter(name=obj.university)
+    #     else:
+    #         return InstituteSerializeParse(Institute.objects.all(),
+    #                                        many=True).data
 
-        return InstituteSerializeParse(
-            Institute.objects.filter(under_campus__in=campus), many=True).data
+        # return InstituteSerializeParse(
+        #     Institute.objects.filter(under_campus__in=campus), many=True).data
 
     class Meta:
-        model = Accounts
+        model = User
         fields = [
             'id', 'eid', 'name', 'designation', 'can_edit', 'campus',
-            'institute'
         ]
+
+class TokenSerialiazer(serializers.ModelSerializer):
+    class Meta:
+        model = Token
+        fields = '__all__'
